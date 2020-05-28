f921873ef358a285ba6b517c848b919603e3ac09
eneas-hsn <eneashsn@gmail.com>
2020-05-28 20:26:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,92 +1,119 @@
- const speed=1
- var speedX= speedY= 0
- var px=py=10
- var tamanhoPeça=20
- var quantidadePeças=20
- var maçaX= maçaY=15
- var rastro=[]
- tail=5
- function hiden() {
-        document
+const speed = 1
+var speedX = speedY = 0
+var positionX = positionY = 10
+var tamanhoPeça = 20
+var quantidadePeças = 20
+var maçaX = maçaY = 15
+var rastro = []
+tail = 5
+function hiden() {
+    document
         .getElementById('home')
         .classList
         .toggle("hide")
-    }
+}
+function hidenover() {
+    document
+        .getElementById('gameover')
+        .classList
+        .remove("hide")
+        
+}
+function começoAutomatico() {
+    speedX = speed
+    speedY = 0
+}
 function gameOver() {
-    speedX=speedY=0
-    tail=5
+    var gameover = document.getElementById('gameover')
+    var points=document.getElementById('points')
+    points.innerText=`${tail} POINTS`
+    points.style.marginLeft="12px"
+    points.style.position="absolute"
+    points.style.marginTop="0px"
+    
 }
+function reset() {
+    hidenover()
+    gameOver()
+    speedX = speedY = 0
+
+}
+
 function gameStart() {
-   hiden()
- var stage=this.document.getElementById("stage")   
- var ctx=stage.getContext("2d")
- this.document.addEventListener("keydown",keyPush)
- this.setInterval(game,60)
- 
- 
- 
- function game() {
-     px+=speedX
-     py+=speedY
-     if (px<0) {
-         px=quantidadePeças-1
-     }
-     if (px>quantidadePeças-1) {
-         px=0
-     }
-     if (py<0) {
-         py=quantidadePeças-1
-     }
-     if (py>quantidadePeças-1) {
-         py=0
-     }
- ctx.fillStyle="#222f3e"
- ctx.fillRect(0,0,stage.width,stage.height)
 
- ctx.fillStyle="red"
- ctx.fillRect(maçaX*tamanhoPeça, maçaY*tamanhoPeça,tamanhoPeça,tamanhoPeça)
+    começoAutomatico()
+    hiden()
+    var stage = this.document.getElementById("stage")
+    var ctx = stage.getContext("2d")
+    this.document.addEventListener("keydown", keyPush)
+    this.setInterval(game, 60)
 
- ctx.fillStyle="gray"
- for (let i = 0; i < rastro.length; i++) {
-    ctx.fillRect(rastro[i].x*tamanhoPeça,rastro[i].y*tamanhoPeça,tamanhoPeça,tamanhoPeça)
-    if (rastro[i].x==px && rastro[i].y==py) {
-        gameOver()
-    } 
+    function game() {
+
+        positionX += speedX
+        positionY += speedY
+        if (positionX < 0) {
+            positionX = quantidadePeças - 1
+        }
+        if (positionX > quantidadePeças - 1) {
+            positionX = 0
+        }
+        if (positionY < 0) {
+            positionY = quantidadePeças - 1
+        }
+        if (positionY > quantidadePeças - 1) {
+            positionY = 0
+        }
+        ctx.fillStyle = "#222f3e"
+        ctx.fillRect(0, 0, stage.width, stage.height)
+
+        ctx.fillStyle = "red"
+        ctx.fillRect(maçaX * tamanhoPeça, maçaY * tamanhoPeça, tamanhoPeça, tamanhoPeça)
+
+        ctx.fillStyle = "gray"
+        for (let i = 0; i < rastro.length; i++) {
+            ctx.fillRect(rastro[i].x * tamanhoPeça, rastro[i].y * tamanhoPeça, tamanhoPeça, tamanhoPeça)
+            if (rastro[i].x == positionX && rastro[i].y == positionY) {
+                reset()
+            }
+        }
+        ctx.fillStyle = "white"
+        ctx.font = "16px COURIER"
+        ctx.fillText("SNAKE GAME", 157, 20)
+
+        ctx.font = "13px courier"
+        ctx.fillText("POINTS " + tail, 324, 389)
+        rastro.push({ x: positionX, y: positionY })
+        while (rastro.length > tail) {
+            rastro.shift()
+        }
+        if (maçaX == positionX && maçaY == positionY) {
+            tail++
+            maçaX = Math.floor(Math.random() * quantidadePeças)
+            maçaY = Math.floor(Math.random() * quantidadePeças)
+        }
+    }
+    function keyPush(event) {
+        switch (event.keyCode) {
+            case 37://left
+                speedX = -speed
+                speedY = 0
+                break;
+            case 38://up
+                speedX = 0
+                speedY = -speed
+                break;
+            case 39://right
+                speedX = speed
+                speedY = 0
+                break;
+            case 40://down
+                speedX = 0
+                speedY = speed
+                break;
+
+            default:
+                break;
+        }
+    }
 }
- ctx.fillStyle="white"
- ctx.font="16px COURIER"
- ctx.fillText("SNAKE GAME",157,20)
- 
- ctx.font="13px courier"
- ctx.fillText("POINTS "+tail,324,389)
-rastro.push({x:px,y:py})
-while (rastro.length>tail) {
-    rastro.shift()
-}
-if (maçaX==px&&maçaY==py) {
-    tail++
-    maçaX=Math.floor(Math.random()*quantidadePeças)
-    maçaY=Math.floor(Math.random()*quantidadePeças)
-}
-}
- function keyPush(event) {
-     switch (event.keyCode) {
-         case 37://left
-             speedX=-speed
-             speedY=0
-             break;
-             case 38://up
-             speedX=0
-             speedY=-speed
-             break;
-             case 39://right
-             speedX=speed
-             speedY=0
-             break;
-             case 40://down
-             speedX=0
-             speedY=speed
-             break;
-     
-         default:
-             break;}}}
