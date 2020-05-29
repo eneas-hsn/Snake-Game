ade35320c25950df114d42bb71c8f661d264422b
eneas-hsn <eneashsn@gmail.com>
2020-05-29 15:11:48 -0300

    Melhor versão diav29/05
    Com todas as funcionalidades, porem ainda vou adicionar um localstorage para gravar os recordes.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var stage = this.document.getElementById("stage")
-    var ctx = stage.getContext("2d")
-    
-    const speed = 1
+var ctx = stage.getContext("2d")
+
+const speed = 1
 var speedX = speedY = 1
 var positionX = positionY = 10
 var tamanhoPeça = 20
@@ -12,11 +12,17 @@ tail = 5
 
 function gameInit() {
     
+    game()
+    começoAutomatico()
+    hiden()
+    this.document.addEventListener("keydown", keyPush)
+    this.y
+    this.setInterval(game, 80)
+    
     document
-        .getElementById('gameover')
+        .getElementById('black')
         .classList
         .add("hide")
-    gameStart()
 }
 function hiden() {
     document
@@ -24,93 +30,90 @@ function hiden() {
         .classList
         .add("hide")
     document
-        .getElementById('gameover')
+        .getElementById('black')
         .classList
         .add("hide")
 }
 function hidenover() {
     document
-        .getElementById('gameover')
+        .getElementById('black')
         .classList
         .remove("hide")
-        
+
 }
 function começoAutomatico() {
     speedX = speed
     speedY = 0
-    tail=5
+    tail = 5
 }
 function gameOver() {
-    var speedX = speedY =0
-
-var maçaX = maçaY = 10
-tail=5
-
-    // var pts=document.getElementById('points')
-    // pts.innerHTML=`${tail} POINTS`
-    //  ctx.fillStyle="black"
-    //  ctx.fillRect(0,0,400,400)
+    var speedX = speedY = 0
+    var maçaX = maçaY = 10
     
+
+    var pts = document.getElementById('points')
+    pts.innerHTML = `${tail} POINTS`
     
+
+
+}
+function game() {
+    positionX += speedX
+    positionY += speedY
+    if (positionX < 0) {
+        positionX = quantidadePeças - 1
+    }
+    if (positionX > quantidadePeças - 1) {
+        positionX = 0
+    }
+    if (positionY < 0) {
+        positionY = quantidadePeças - 1
+    }
+    if (positionY > quantidadePeças - 1) {
+        positionY = 0
+    }
+    ctx.fillStyle = "#222f3e"
+    ctx.fillRect(0, 0, stage.width, stage.height)
+
+    ctx.fillStyle = "red"
+    ctx.fillRect(maçaX * tamanhoPeça, maçaY * tamanhoPeça, tamanhoPeça, tamanhoPeça)
+
+    ctx.fillStyle = "gray"
+    for (let i = 0; i < rastro.length; i++) {
+        ctx.fillRect(rastro[i].x * tamanhoPeça, rastro[i].y * tamanhoPeça, tamanhoPeça, tamanhoPeça)
+        if (rastro[i].x == positionX && rastro[i].y == positionY) {
+            speedX = speedY = 0
+
+            hidenover()
+            gameOver()
+        }
+    }
+
+    ctx.fillStyle = "white"
+    ctx.font = "16px COURIER"
+    ctx.fillText("SNAKE GAME", 157, 20)
+
+    ctx.font = "13px courier"
+    ctx.fillText("POINTS " + tail, 324, 389)
+    rastro.push({ x: positionX, y: positionY })
+    while (rastro.length > tail) {
+        rastro.shift()
+    }
+    if (maçaX == positionX && maçaY == positionY) {
+        tail++
+        maçaX = Math.floor(Math.random() * quantidadePeças)
+        maçaY = Math.floor(Math.random() * quantidadePeças)
+    }
 }
 
-
 function gameStart() {
-    
+    game()
     começoAutomatico()
     hiden()
     this.document.addEventListener("keydown", keyPush)
-    this.setInterval(game,80)
+    this.y
+    this.setInterval(game, 80)
 
-    function game() {
-
-        positionX += speedX
-        positionY += speedY
-        if (positionX < 0) {
-            positionX = quantidadePeças - 1
-        }
-        if (positionX > quantidadePeças - 1) {
-            positionX = 0
-        }
-        if (positionY < 0) {
-            positionY = quantidadePeças - 1
-        }
-        if (positionY > quantidadePeças - 1) {
-            positionY = 0
-        }
-        ctx.fillStyle = "#222f3e"
-        ctx.fillRect(0, 0, stage.width, stage.height)
-
-        ctx.fillStyle = "red"
-        ctx.fillRect(maçaX * tamanhoPeça, maçaY * tamanhoPeça, tamanhoPeça, tamanhoPeça)
-
-        ctx.fillStyle = "gray"
-        for (let i = 0; i < rastro.length; i++) {
-            ctx.fillRect(rastro[i].x * tamanhoPeça, rastro[i].y * tamanhoPeça, tamanhoPeça, tamanhoPeça)
-            if (rastro[i].x == positionX && rastro[i].y == positionY) {
-                speedX = speedY = 0
-                
-                hidenover()
-    gameOver()
-            }
-        }
-        
-        ctx.fillStyle = "white"
-        ctx.font = "16px COURIER"
-        ctx.fillText("SNAKE GAME", 157, 20)
-
-        ctx.font = "13px courier"
-        ctx.fillText("POINTS " + tail, 324, 389)
-        rastro.push({ x: positionX, y: positionY })
-        while (rastro.length > tail) {
-            rastro.shift()
-        }
-        if (maçaX == positionX && maçaY == positionY) {
-            tail++
-            maçaX = Math.floor(Math.random() * quantidadePeças)
-            maçaY = Math.floor(Math.random() * quantidadePeças)
-        }
-    }
     function keyPush(event) {
         switch (event.keyCode) {
             case 37://left
@@ -129,7 +132,6 @@ function gameStart() {
                 speedX = 0
                 speedY = speed
                 break;
-
             default:
                 break;
         }
